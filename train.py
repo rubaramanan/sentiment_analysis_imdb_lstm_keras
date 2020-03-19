dc7ef766064df55a278d0434c19248ea1a10bc5c
ThayaRamanan <rubaramanan1997@gmail.com>
2020-03-19 15:50:03 +0530

    train model

--- train.py
+++ train.py
@@ -20,6 +20,7 @@ def get_array_from_directory(path):
         data = f.read()
         words = text_to_word_sequence(data)
         result = one_hot(data, round(len(words)*1.3))
+        # print(result)
         m.append(result)
 
     return m
@@ -28,22 +29,25 @@ def get_array_from_directory(path):
 posarray = get_array_from_directory(pospath)
 negarray = get_array_from_directory(negpath)
 
-poslabelarr=np.zeros(len(posarray))
-neglabelarr=np.ones(len(negarray))
+poslabelarr = np.zeros(12500)
+neglabelarr = np.ones(12500)
+
+for i in range(len(negarray)):
+    posarray.append(negarray[i])
 
 
-for i in range(len(posarray)):
-    negarray.append(posarray[i])
-
-
-features=negarray
+features = posarray
 features = pad_sequences(features, padding='post', maxlen=2000)
-features = np.array(features).astype('f')
+# features = np.array(features).astype('f')
+# features=np.concatenate((posarray,negarray))
 
-labels = np.concatenate((poslabelarr,neglabelarr), axis=None)
-# labels = keras.utils.to_categorical(labels)
+labels = np.concatenate((poslabelarr,neglabelarr))
+labels = keras.utils.to_categorical(labels,2)
 
-
+# print(features)
+# print(len(features))
+# print(len(features[0]))
+# print(labels)
 
 model = Sequential()
 # model.add(Embedding(2000,128))
@@ -57,9 +61,10 @@ model = Sequential()
 
 model.add(Embedding(2000, 128))
 model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(2, activation='softmax'))
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
 print(model.summary())
 
 model.fit(features,labels,epochs=25)
